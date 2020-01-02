69c76ccb6cff8b550f2bd08322e36b2b06e42f58
imkumaraju <imkumaraju@gmail.com>
2020-01-02 18:34:51 +0530

    first commit

---
+++ src/modules/movie-browser/movie-browser.container.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Container, Row} from 'react-bootstrap';
+import {AppBar} from 'material-ui';
+import * as movieActions from './movie-browser.actions';
+import * as movieHelpers from './movie-browser.helpers';
+import MovieList from './movie-list/movie-list.component';
+import * as scrollHelpers from '../common/scroll.helpers';
+
+class MovieBrowser extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: 1
+    };
+    // Binds the handleScroll to this class (MovieBrowser)
+    // which provides access to MovieBrowser's props
+    // Note: You don't have to do this if you call a method
+    // directly from a lifecycle method or define an arrow function
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.onscroll = this.handleScroll;
+    this.props.getTopMovies(this.state.currentPage);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const {topMovies} = this.props;
+    if (!topMovies.isLoading) {
+      let percentageScrolled = scrollHelpers.getScrollDownPercentage(window);
+      if (percentageScrolled > .8) {
+        const nextPage = this.state.currentPage + 1;
+        this.props.getTopMovies(nextPage);
+        this.setState({currentPage: nextPage});
+      }
+    }
+  }
+
+  render() {
+    const {topMovies} = this.props;
+    const movies = movieHelpers.getMoviesList(topMovies.response);
+
+    return (
+      <div>
+        <AppBar title='Find My Movie' />
+        <Container>
+          <Row>
+            <p>Search will go here</p>
+          </Row>
+          <Row>
+            <MovieList movies={movies} isLoading={topMovies.isLoading} />
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  // Map nodes in our state to a properties of our component
+  (state) => ({
+    topMovies: state.movieBrowser.topMovies
+  }),
+  // Map action creators to properties of our component
+  { ...movieActions }
+)(MovieBrowser);
